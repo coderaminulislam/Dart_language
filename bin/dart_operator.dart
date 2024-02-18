2ef06262fda4f6344a0d17684ac48c03f35ea957
coderaminulislam <aminulwife.taslima@gmail.com>
2024-02-18 23:02:22 +0600

    new file

---
+++ bin/dart_operator.dart
@@ -0,0 +1,29 @@
+main(){
+  // operator
+
+  int a = 6;
+  int b = 4;
+
+  print(a+b);
+  print(a-b);
+  print(a*b);
+  print(a/b);
+  print(a%b);
+
+  // prefix and postfix
+
+  // print(a++);
+  // print (++a);
+
+
+  // print(--a);
+  // print(--a);
+  // print(a);
+
+  a += 10;
+  print(a);
+
+  a -= 3;
+  print(a);
+
+}
---
+++ bin/if_else.dart
@@ -0,0 +1,12 @@
+main(){
+  int money = 1;
+  if(money> 20){
+    print("I go to market");
+
+  } else if(money > 10){
+    print('I go to shop');
+  }
+  else{
+    print("I stay at home");
+  }
+}
