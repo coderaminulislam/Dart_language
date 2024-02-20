840b699ed560965bf87a267d902c087a4b21ba75
coderaminulislam <aminulwife.taslima@gmail.com>
2024-02-20 22:31:48 +0600

    update

---
+++ bin/looping.dart
@@ -0,0 +1,21 @@
+main(){
+  //for loop
+
+  // for ( int i=0; i<=100; i= i+1){
+  //  if(i==0){
+  //    continue;
+  //  }
+  //  if( i==50){
+  //   break;
+  //  }
+  //  print('ami asi $i;');
+  // }
+  // print('done');
+
+  List<String> studentList = ['Aminul', 'Rakib', 'gofur', 'Abdul'];
+
+  for (int i = 0; i<studentList.length; i++){
+    print("Student Name ${studentList[i]};");
+  }
+
+}
